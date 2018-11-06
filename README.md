1aa3aefadee87ded9f239491e42324d274e3f57a
eigenroy <mjroyster@gmail.com>
2018-11-06 15:36:33 -0600

    Rolled as package.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 
-This is a test.
+Install with:
+
+python setup.py install
---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='testPack',
+      version='0.1dev',
+      description='Testing rolling a package',
+      author='Marc Royster',
+      author_email='mjroyster@gmail.com',
+      packages=['testPack'],
+     ) #complete
